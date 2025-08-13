b7e06abcd1cb4da5330a370a724185a13cfde25f
Jo0kker <boublou@hotmail.fr>
2025-08-13 15:18:13 +0200

    update deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        // Trust all proxies for HTTPS detection
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PREFIX);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
